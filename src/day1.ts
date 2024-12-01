64a47c89b8cc48c4d44f028ddec3636a1ecd26e2
Shivan <shivantheman@gmail.com>
2024-12-01 22:57:12 +0200

    attempt 2

--- src/day1.ts
+++ src/day1.ts
@@ -5,19 +5,35 @@ export const day1 = async () => {
   const left: number[] = [];
   const right: number[] = [];
 
-  for (const line of lines) {
-    const [leftNum, rightNum] = line.trim().split(/\s+/).map(Number);
-    left.push(leftNum);
-    right.push(rightNum);
-  }
+  // for (const line of lines) {
+  //   const [leftNum, rightNum] = line.trim().split(/\s+/).map(Number);
+  //   left.push(leftNum);
+  //   right.push(rightNum);
+  // }
 
-  left.sort((a, b) => a - b);
-  right.sort((a, b) => a - b);
+  // left.sort((a, b) => a - b);
+  // right.sort((a, b) => a - b);
 
-  let total = 0;
-  for (let i = 0; i < left.length; i++) {
-    total += Math.abs(left[i] - right[i]);
-  }
+  // let total = 0;
+  // for (let i = 0; i < left.length; i++) {
+  //   total += Math.abs(left[i] - right[i]);
+  // }
+
+  // Alternatively...
+  const total =
+    lines
+      .map((line) => line.trim().split(/\s+/).map(Number))
+      .reduce((acc, [leftNum, rightNum]) => {
+        left.push(leftNum);
+        right.push(rightNum);
+        return acc;
+      }, 0) +
+    left
+      .sort((a, b) => a - b)
+      .reduce(
+        (acc, num, i) => acc + Math.abs(num - right.sort((a, b) => a - b)[i]),
+        0
+      );
 
   console.log("distance", total);
 
